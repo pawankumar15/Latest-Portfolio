279c36f5887214bf9a613b9a1067782274fa3caf
Pawan Kumar <124373810+pawankumar15@users.noreply.github.com>
2024-02-26 11:18:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['Frontend Developer', 'YouTuber', 'Web Developer'],
+    typeSpeed: 50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+  });
